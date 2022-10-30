eec338841950672fa662b1f810351d4b89d7944f
伊藤 駿 <rendya2501@gmail.com>
2022-10-30 15:48:26 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorServerCRUD/Program.cs
@@ -0,0 +1,36 @@
+using BlazorServerCRUD.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+// データコンテキストサービスを登録
+builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("videogamedb"));
+// ゲームサービスの登録
+builder.Services.AddScoped<IGameService, GameService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
